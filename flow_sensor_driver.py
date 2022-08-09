52fedf89c481cd9a25a6b35565e9a5ed762d989b
unknown <tooles01@nyu.edu>
2022-08-09 14:44:13 -0400

    adds calibration table selection
    removes raw data display box

--- flow_sensor_driver.py
+++ flow_sensor_driver.py
@@ -1,19 +1,18 @@
 # flowSensor_driver.py
 # for Honeywell 5100V
 
-#import os, time, serial
-import sys
-import logging
+import os, sys, logging, csv
 from PyQt5 import QtCore, QtSerialPort
 from PyQt5.QtWidgets import *
 from serial.tools import list_ports
-import utils
+import utils, utils_olfa_48line
 
 currentDate = utils.currentDate
 noPortMsg = ' ~ No COM ports detected ~'
 
 flowSens_baud = 9600
-sensorType = "Honeywell 5101V"
+sensorType = "Honeywell 5101V"  # TODO
+calibration_table_item_number = 4
 
 
 # CREATE LOGGER
@@ -28,23 +27,26 @@ class flowSensor(QGroupBox):
     def __init__(self, port=""):
         super().__init__()
         self.port = port
-
         self.connected = False
-
+        
         self.createConnectBox()
         self.create_settings_box()
-        self.create_raw_data_box()
+        self.create_cal_table_box()
         self.createDataReceiveBoxes()
 
         mainLayout = QGridLayout()
-        mainLayout.addWidget(self.connectBox,0,0,1,1)
-        mainLayout.addWidget(self.settings_box,1,0,1,1)
-        mainLayout.addWidget(self.raw_data_box,2,0,1,1)
+        mainLayout.addWidget(self.connectBox,0,0,)
+        mainLayout.addWidget(self.settings_box,1,0)
+        mainLayout.addWidget(self.cal_table_box,2,0)
         mainLayout.addWidget(self.dataReceiveBox,0,1,3,1)
         self.setLayout(mainLayout)
+
+        self.settings_box.setMaximumHeight(self.settings_box.sizeHint().height())
+        self.cal_table_widget.setFixedHeight(100)
+        self.cal_table_box.setFixedHeight(self.cal_table_box.sizeHint().height())
+        self.dataReceiveBox.setFixedWidth(self.dataReceiveBox.sizeHint().width())
     
-    
-    # CONNECT TO DEVICE
+    # CREATE GUI ELEMENTS
     def createConnectBox(self):
         self.connectBox = QGroupBox("Connect")
 
@@ -54,45 +56,122 @@ class flowSensor(QGroupBox):
         self.refreshButton = QPushButton(text="Refresh",clicked=self.getPorts)
         self.getPorts()
 
-        #readLbl = QLabel(text="raw data from serial port:")
-        #self.rawReadDisplay = QTextEdit(readOnly=True)
-        #self.rawReadSpace = QWidget()
-        #readLayout = QVBoxLayout()
-        #readLayout.addWidget(readLbl)
-        #readLayout.addWidget(self.rawReadDisplay)
-        #self.rawReadSpace.setLayout(readLayout)
-
         self.connectBoxLayout = QFormLayout()
         self.connectBoxLayout.addRow(self.portLbl,self.port_widget)
         self.connectBoxLayout.addRow(self.refreshButton,self.connectButton)
-        #self.connectBoxLayout.addRow(self.rawReadSpace)
         self.connectBox.setLayout(self.connectBoxLayout)
 
     def create_settings_box(self):
         self.settings_box = QGroupBox('Settings')
-
+        
         self.timebtreqs_wid = QLineEdit(text='100',returnPressed=lambda:self.send_to_arduino('MM_timebt_' + self.timebtreqs_wid.text()))
         self.timebtreqs_btn = QPushButton(text="Send", clicked=lambda:self.send_to_arduino('MM_timebt_' + self.timebtreqs_wid.text()))
         
         layout = QHBoxLayout()
-        layout.addWidget(QLabel(text="Time b/t requests for slave data (ms):"))
+        layout.addWidget(QLabel(text="Time b/t acquisitions (ms):"))
         layout.addWidget(self.timebtreqs_wid)
         layout.addWidget(self.timebtreqs_btn)
         self.settings_box.setLayout(layout)
-
-
-    def create_raw_data_box(self):
-        self.raw_data_box = QGroupBox('Raw Data')
-
-        readLbl = QLabel(text="raw data from serial port:")
-        self.rawReadDisplay = QTextEdit(readOnly=True)
-        self.rawReadSpace = QWidget()
+    
+    def create_cal_table_box(self):
+        self.cal_table_box = QGroupBox('Calibration table')
+        
+        self.get_calibration_tables()
+        self.cal_table_widget = QListWidget()
+        self.cal_table_widget.addItems(self.sccm2Ard_dicts)
+        default_cal_table_item = self.cal_table_widget.item(calibration_table_item_number)
+        self.cal_table_widget.setCurrentItem(default_cal_table_item)
+        
+        self.cal_table_btn = QPushButton('Set calibration table',checkable=True,toggled=self.cal_tbl_btn_toggled)
+        if self.cal_table_btn.isChecked() == False:
+            self.cal_table_btn.toggle()
         
         layout = QVBoxLayout()
-        layout.addWidget(readLbl)
-        layout.addWidget(self.rawReadDisplay)
-        self.raw_data_box.setLayout(layout)
+        layout.addWidget(self.cal_table_widget)
+        layout.addWidget(self.cal_table_btn)
+        self.cal_table_box.setLayout(layout)
     
+    def createDataReceiveBoxes(self):
+        self.dataReceiveBox = QGroupBox("data received")
+
+        self.receiveBox = QTextEdit(readOnly=True)
+        receiveBoxLbl = QLabel(text="Flow val (int), Flow val (SCCM)")
+        receiveBoxLayout = QVBoxLayout()
+        receiveBoxLayout.addWidget(receiveBoxLbl)
+        receiveBoxLayout.addWidget(self.receiveBox)
+
+        layout = QHBoxLayout()
+        layout.addLayout(receiveBoxLayout)
+        self.dataReceiveBox.setLayout(layout)
+    
+    
+    # FUNCTIONS
+    def get_calibration_tables(self):
+        self.flow_cal_dir = utils.find_olfaControl_directory() + '\\calibration_tables' # NOTE: this takes a super long time
+        
+        if os.path.exists(self.flow_cal_dir):
+            logger.debug('loading flow sensor calibration tables (%s)', self.flow_cal_dir)
+
+            file_type = '.txt'
+            cal_file_names = os.listdir(self.flow_cal_dir)
+            cal_file_names = [fn for fn in cal_file_names if fn.endswith(file_type)]    # only txt files # TODO: change to csv
+            if cal_file_names == []:
+                logger.warning('no cal files found')
+            else:
+                # create dictionaries
+                new_sccm2Ard_dicts = {}
+                new_ard2Sccm_dicts = {}
+                for cal_file in cal_file_names:
+                    x = 0       # what is this
+                    idx_ext = cal_file.find('.')
+                    file_name = cal_file[:idx_ext]
+                    sccm2Ard = {}
+                    ard2Sccm = {}
+                    cal_file_full_dir = self.flow_cal_dir + '\\' + cal_file
+                    with open(cal_file_full_dir, newline='') as f:
+                        csv_reader = csv.reader(f)
+                        firstLine = next(csv_reader)    # skip over header line
+                        # get the shit
+                        reader = csv.DictReader(f, delimiter=',')
+                        for row in reader:
+                            if x == 0:
+                                try:
+                                    sccm2Ard[int(row['SCCM'])] = int(row['int'])
+                                    ard2Sccm[int(row['int'])] = int(row['SCCM'])
+                                except KeyError as err:
+                                    logger.warning('error: %s',err)
+                                    logger.warning('%s does not have correct headings for calibration files', cal_file)
+                                    x = 1   # don't keep trying to read this file
+                    if bool(sccm2Ard) == True:
+                        new_sccm2Ard_dicts[file_name] = sccm2Ard
+                        new_ard2Sccm_dicts[file_name] = ard2Sccm
+                
+                # if new dicts are not empty, replace the old ones
+                if len(new_sccm2Ard_dicts) != 0:
+                    self.sccm2Ard_dicts = new_sccm2Ard_dicts
+                    self.ard2Sccm_dicts = new_ard2Sccm_dicts
+                else:
+                    logger.info('no calibration files found in this directory')
+            
+        else:
+            logger.warning('Cannot find flow cal directory (searched in %s)', self.flow_cal_dir)   # TODO this is big issue if none found
+    
+    def cal_tbl_btn_toggled(self, checked):
+        if checked:
+            self.cal_table_btn.setText('Change calibration table')
+            self.cal_table_widget.setEnabled(False)
+            self.calibration_table_changed()
+        else:
+            self.cal_table_btn.setText('Set calibration table')
+            self.cal_table_widget.setEnabled(True)
+    
+    def calibration_table_changed(self):
+        new_calibration_table_name = self.cal_table_widget.currentItem().text()
+        self.intToSccm_dict = self.ard2Sccm_dicts.get(new_calibration_table_name)
+        self.sccmToInt_dict = self.sccm2Ard_dicts.get(new_calibration_table_name)
+    
+    
+    # CONNECT TO DEVICE
     def getPorts(self):
         self.port_widget.clear()
         ports = list_ports.comports()
@@ -114,7 +193,6 @@ class flowSensor(QGroupBox):
             if 'Arduino' in this_item:
                 break
         if item_idx != []:
-            logger.debug('setting olfa port widget to arduino port')
             self.port_widget.setCurrentIndex(item_idx)
         else:
             logger.debug('no Arduinos detected :(')
@@ -164,32 +242,8 @@ class flowSensor(QGroupBox):
             self.connectButton.setChecked(False)
             self.refreshButton.setEnabled(True)
     
-    def send_to_arduino(self, strToSend):
-        bArr_send = strToSend.encode()
-        try:
-            if self.serial.isOpen():
-                self.serial.write(bArr_send)                # send to Arduino
-                #self.raw_write_display.append(strToSend)    # display string that was sent
-            else:
-                logger.warning('Serial port not open, cannot send parameter: %s', strToSend)
-        except AttributeError as err:
-            logger.warning('(Attribute Error) Serial port not open, cannot send parameter: %s', strToSend)
-
     
-    # RECEIVE DATA
-    def createDataReceiveBoxes(self):
-        self.dataReceiveBox = QGroupBox("data received")
-
-        self.receiveBox = QTextEdit(readOnly=True)
-        receiveBoxLbl = QLabel(text="Flow val (int), Flow val (SCCM)")
-        receiveBoxLayout = QVBoxLayout()
-        receiveBoxLayout.addWidget(receiveBoxLbl)
-        receiveBoxLayout.addWidget(self.receiveBox)
-
-        layout = QHBoxLayout()
-        layout.addLayout(receiveBoxLayout)
-        self.dataReceiveBox.setLayout(layout)
-    
+    # SEND/RECEIVE DATA
     def receive(self):
         if self.serial.canReadLine() == True:
             text = self.serial.readLine(1024)
@@ -199,21 +253,28 @@ class flowSensor(QGroupBox):
                 self.rawReadDisplay.append(text)
                 if text.isnumeric():
                     str_value = text
-                    flowVal = int(text)
-                    #val_SCCM = utils.convertToSCCM(flowVal,sensorType)
-                    #dataStr = str_value + '\t' + str(val_SCCM)
-                    dataStr = str_value + '\t' + str(flowVal)
+                    flowVal_int = int(text)
+                    val_SCCM = utils_olfa_48line.convertToSCCM(flowVal_int,self.intToSccm_dict)
+                    dataStr = str_value + '\t' + str(val_SCCM)
+                    #dataStr = str_value + '\t' + str(flowVal_int)
                     self.receiveBox.append(dataStr)
-                    try:
-                        self.window().receive_data_from_device('flow sensor','FL',str_value)
-                    except AttributeError as err:
-                        pass
+                    try: self.window().receive_data_from_device('flow sensor','FL',str_value)
+                    except AttributeError as err: pass
             except UnicodeDecodeError as err:
                 logger.error('Serial read error: %s',err)
+    
+    def send_to_arduino(self, strToSend):
+        bArr_send = strToSend.encode()
+        try:
+            if self.serial.isOpen():
+                self.serial.write(bArr_send)                # send to Arduino
+            else:
+                logger.warning('Serial port not open, cannot send parameter: %s', strToSend)
+        except AttributeError as err:
+            logger.warning('(Attribute Error) Serial port not open, cannot send parameter: %s', strToSend)
 
 
 if __name__ == "__main__":
-    logger.debug('opening window')
     app1 = QApplication(sys.argv)
     theWindow = flowSensor()
     theWindow.show()
