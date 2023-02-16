1b5ff1ba00488d49cd197d5aa8b0a75ad6bc0fe9
Shannon Toole <66132201+tooles01@users.noreply.github.com>
2023-02-15 23:57:37 -0500

    adds more descriptive comments

--- utils.py
+++ utils.py
@@ -2,6 +2,8 @@ import logging, os, glob
 from datetime import datetime
 
 
+
+#################################
 # TIME
 currentDate = str(datetime.date(datetime.now()))
 
@@ -13,8 +15,16 @@ def get_current_time():
     return current_time_str
 
 
+
+#################################
 # LOGGING
-def create_file_handler(log_dir):    # TODO: add file_name to variables it takes
+def create_file_handler(log_dir):
+    '''
+    Returns file handler for logger: 'logfile_<today's date>.txt'
+        Level set to 'INFO'
+        Path is log_dir
+    '''
+
     file_name = 'logfile_{}.txt'.format(currentDate)
     logFilePath = log_dir + '\\' + file_name
     
@@ -23,68 +33,98 @@ def create_file_handler(log_dir):    # TODO: add file_name to variables it takes
     file_handler = logging.FileHandler(logFilePath,mode='a')
     file_handler.setLevel(file_handler_level)
     file_handler.setFormatter(file_handler_formatter)
-
+    
     return file_handler
 
 def create_console_handler():   # TODO: user sends log level to this function
+    '''
+    Returns console handler for logger
+        Set level to 'DEBUG'
+    '''
+    
     console_handler_level = logging.DEBUG
     console_handler_formatter = logging.Formatter('%(name)-14s: %(levelname)-8s: %(message)s')
 
     console_handler = logging.StreamHandler()
     console_handler.setLevel(console_handler_level)
     console_handler.setFormatter(console_handler_formatter)
-
+    
     return console_handler
 
 
 
+#################################
 # FIND DIRECTORIES
 def find_olfaControl_directory():
     str_to_find = '/**/*OlfaControl_GUI*'
     
-    # search C:\\GIT for OlfaControl_GUI
-    c_drive_path = os.path.expanduser('C:\\GIT\\')
-    path_list = glob.glob(c_drive_path + '/**/*' + str_to_find,recursive=True)
-    if not path_list:
-        # search in Dropbox
-        c_drive_path = os.path.expanduser('~\\Dropbox')
-        path_list = glob.glob(c_drive_path + '/**/*' + str_to_find,recursive=True)
-        if not path_list:
-            # search in Dropbox again
-            c_drive_path = os.path.expanduser('~\\Dropbox (NYU Langone Health)')
-            path_list = glob.glob(c_drive_path + str_to_find,recursive=True)
+    # Search C:\\GIT for paths containing OlfaControl_GUI
+    c_drive_git_path = os.path.expanduser('C:\\GIT\\')
+    path_list = glob.glob(c_drive_git_path + '/**/*' + str_to_find,recursive=True)
     
-    # if you can't find it anywhere
+    # If not found: search Dropbox
+    if not path_list:
+        c_drive_git_path = os.path.expanduser('~\\Dropbox')
+        path_list = glob.glob(c_drive_git_path + '/**/*' + str_to_find,recursive=True)
+        '''
+        #if not path_list:
+        #    # search in Dropbox again
+        #    c_drive_git_path = os.path.expanduser('~\\Dropbox (NYU Langone Health)')
+        #    path_list = glob.glob(c_drive_git_path + str_to_find,recursive=True)
+        '''
+    
+    # If not found: search Dropbox (NYU Langone Health)
+    if not path_list:
+        c_drive_git_path = os.path.expanduser('~\\Dropbox (NYU Langone Health)')
+        path_list = glob.glob(c_drive_git_path + str_to_find, recursive=True)
+    
+    # If not found: print warning
     if not path_list:
         gui_directory = []
         logger.warning('can\'t find OlfaControl_GUI folder :/')
+    
+    # If found: use the first path listed
     else:
+        # If multiple paths found: print (for user)
+        if len(path_list) > 1:
+            logger.debug('Multiple OlfaControl_GUI paths found:')
+            for i in path_list:
+                logger.debug('\t' + i)
+        
         gui_directory = path_list[0]
+        logger.info('OlfaControl_GUI directory found at:\t' + gui_directory)
     
     return gui_directory
 
 def find_datafile_directory():
     folder_to_save_to = 'result_files'
     
+    # Find OlfaControl_GUI directory
     olfaControl_dir = find_olfaControl_directory()
+    
+    # If not found: save to C:\\
     if not olfaControl_dir:
         # yolo
-        c_drive_path = os.path.expanduser('C:\\')
-        save_files_to = c_drive_path + '\\' + folder_to_save_to
-        #if not os.path.exists(save_files_to): os.mkdir(save_files_to)
+        c_drive_git_path = os.path.expanduser('C:\\')
+        save_files_to = c_drive_git_path + '\\' + folder_to_save_to    
+    
+    # If found:
     else:
         save_files_to = olfaControl_dir + '\\' + folder_to_save_to
-    #save_files_to = c_drive_path + '\\' + folder_to_save_to
-    if not os.path.exists(save_files_to): os.mkdir(save_files_to)
-
-    #logger.debug('saving result files to %s',save_files_to)
     
+    '''
+    # If folder does not exist, create it
+    if not os.path.exists(save_files_to):
+        os.mkdir(save_files_to)
+    '''
+    
+    #logger.info('saving result files to %s',save_files_to)
     return save_files_to
 
 
 
 
-############################################
+#################################
 logger = logging.getLogger(name='utils')
 logger.setLevel(logging.DEBUG)
 if logger.hasHandlers():    logger.handlers.clear()     # removes duplicate log messages
