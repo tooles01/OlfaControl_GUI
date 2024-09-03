3e2ddf86e29f2997cfbce5204ce3d04508eac15d
Shannon Toole <tooles01@nyu.edu>
2024-09-03 15:48:03 -0400

    Updates to analysis functions: more descriptive comments

--- analysis/functions/import_datafile.m
+++ analysis/functions/import_datafile.m
@@ -1,37 +1,31 @@
 %% import_datafile
-% get datafile (.csv) from results folder, save in matlab folder as .mat file
-% purpose is to speed up processing - don't need to re-import the whole thing every single time
+% get datafile (*.csv) from results folder, save within analysis folder as *.mat file
+% purpose is to speed up processing - don't need to re-import the whole *.csv every single time
 
 % ***assumes current working directory is 'OlfaControl_GUI\analysis'
 
 %%
 % Inputs: 
-%   - file name         (ex: '2022-09-13_datafile_02')
-%   - file directory    (ex: 'C:\Users\Admin\Dropbox (NYU Langone Health)\OlfactometerEngineeringGroup (2)\Control\a_software\OlfaControl_GUI\result_files\48-line olfa\2022-09-13\')
+%   - File name         (ex: '2022-09-13_datafile_02')
+%   - File directory    (ex: 'C:\Users\Admin\Dropbox (NYU Langone Health)\OlfactometerEngineeringGroup (2)\Control\a_software\OlfaControl_GUI\result_files\48-line olfa\2022-09-13\')
 % Output:
-%   - saves .mat file to 'analysis\data files (raw)'
-%   - returns cell array with the contents of the .mat file
+%   - Saves *.mat file to 'analysis\data files (raw)'
+%   - Returns cell array with the contents of the *.mat file
 
 %%
 function this_mat_file_data = import_datafile(file_name, file_directory)
     dir_datafiles_matlab = strcat(pwd,'\','data files (raw)');
     dir_thisfile_full = strcat(dir_datafiles_matlab,'\',file_name,'.mat');
     
-    % if .mat file does not exist yet, get the .csv and save it as a .mat file
     if ~isfile(dir_thisfile_full)
-        
-        dir_this_data_file = strcat(file_directory,file_name,'.csv');                           % full directory for this file
-        raw_wholeFile = readcell(dir_this_data_file,'LineEnding',{'\r' '\n'},'Delimiter',',');  % read file & save to matlab folder
-        
-        %{
-        if isfile(dir_thisfile_full)
-            delete(dir_thisfile_full)   % make sure this file doesn't already exist (or else it gets in a weird endless loop)
-        end
-        %}
-        save(dir_thisfile_full,"raw_wholeFile")     % save as .mat file
+        % If *.mat file does not exist yet, get the *.csv (& save as a *.mat file)
+        dir_this_data_file = strcat(file_directory,file_name,'.csv');                           % Full directory for this file
+        raw_wholeFile = readcell(dir_this_data_file,'LineEnding',{'\r' '\n'},'Delimiter',',');  % Read file & save to matlab folder
+        save(dir_thisfile_full,"raw_wholeFile")     % Save as .mat file
         this_mat_file_data = raw_wholeFile;
     else
+        % If *.mat file already exists, load it in
         this_mat_file_data = load(dir_thisfile_full);
-        this_mat_file_data = this_mat_file_data.raw_wholeFile;  % get the cell array from the struct
+        this_mat_file_data = this_mat_file_data.raw_wholeFile;  % Get the cell array from the struct
     end
 end
--- analysis/functions/removeDuplicates_.m
+++ analysis/functions/removeDuplicates_.m
@@ -1,44 +1,46 @@
 %% removeDuplicates_.m
-% if there are duplicate entries for the same time value, average them
+% If there are duplicate entries for the same time value, average them
 
 % **assumes data is already sorted
 
 %%
 %#ok<*AGROW>
 
-%%
 function newPdata = removeDuplicates_(pData)
-    
     newPdata = pData;
     numVals = length(pData);
-    [~,ind] = unique(pData(:,1),'rows','first');    % find unique values
-    blankSet = 1:length(pData);
-    dup_ind(:,1) = setdiff(blankSet,ind);           % find duplicate values
-    
-    %% if there are duplicate values:
-    if ~isempty(dup_ind)
 
-        % average duplicates & add to "newPData"; set the duplicate indices to Nan
+    % Find unique values
+    [~,ind] = unique(pData(:,1),'rows','first');
+    blankSet = 1:length(pData);
+    % Find indices of duplicate time values
+    dup_ind(:,1) = setdiff(blankSet,ind);
+    
+    %% If there are duplicate values:
+    if ~isempty(dup_ind)
+        % Average duplicates & add to "newPData"; set the duplicate indices to Nan
+        
+        % For each duplicate value
         for i=1:length(dup_ind)
-            idx_start = dup_ind(i)-1;       % duplicates start at 1-value
-            time_0 = pData(idx_start,1);    % time value
+            idx_start = dup_ind(i)-1;       % Index of the (first) duplicate value
+            time_0 = pData(idx_start,1);    % Time value here
             
-            % if we haven't already done this one
+            % If we haven't already done this one
             if ~isnan(newPdata(idx_start,1))
                 valsToAvg = pData(idx_start,2);
                 idx_next = idx_start+1;
-                time_next = pData(idx_next,1);
+                time_next = pData(idx_next,1);  % This should be the same since it's a duplicate lol
 
-                % while we increment & still have the same time value
+                % While we increment & still have the same time value
                 while time_0 == time_next
 
-                    % if we're not at the end of the data, add it to valsToAvg
+                    % If we're not at the end of the data, add column2 value to valsToAvg
                     if idx_next <= numVals
                         valToAvg_next = pData(idx_next,2);
                         valsToAvg = [valsToAvg;valToAvg_next];
                         idx_next = idx_next+1;
 
-                        % if we're not at the end of the data, check the next time value
+                        % If we're not at the end of the data, check if the next time value is also a duplicate
                         if idx_next <= numVals
                             time_next = pData(idx_next,1);
                         else
@@ -51,12 +53,12 @@ function newPdata = removeDuplicates_(pData)
 
                 end
 
-                % average values, add to newPdata
+                % Average the values, add to newPdata
                 val_avg = mean(valsToAvg);
                 newPdata(idx_start,1) = time_0;
                 newPdata(idx_start,2) = val_avg;
 
-                % set the spaces we removed to "NaN" (to pull out after)
+                % Set the extra (duplicate) data points to "NaN" (to pull out after)
                 j_beg = idx_start+1;
                 j_end = idx_next-1;
                 for j=j_beg:j_end
@@ -68,7 +70,7 @@ function newPdata = removeDuplicates_(pData)
 
     end
     
-    %% remove Nans
+    %% Remove Nans
     nan_idx = all(~isnan(newPdata),2);
     newPdata = newPdata(nan_idx,:);
 
